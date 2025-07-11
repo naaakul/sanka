d5f84e3a58b5f6eab8dc86de66c9dd0fb8ecd838
Nakul Chouksey <71.nakul@gmail.com>
2025-07-11 19:06:54 +0200

    reset: landing

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,9 @@ import Hero from "@/components/session/Hero";
 
 const page = () => {
   return (
-    <div className="min-h-screen w-full relative text-[#f8faf933]">
-      <div className="h-full w-full z-10 absolute bg-[radial-gradient(220%_100%_at_50%_100%,_#092934,_#092934,_#1D4048,_#436067,_#698086,_#A4B2B4,_#E2E5E6,_#F8FAF9)]"></div>
-      <div className="h-full w-full z-10 absolute bg-[linear-gradient(to_right,#f8faf933_1px,transparent_1px),linear-gradient(to_bottom,#f8faf933_1px,transparent_1px)] bg-[size:10px_10px] overflow-hidden"></div>
+    <div className="min-h-screen w-full">
       <Navbar/>
       <Hero/>
-
-
-
-
-
-
-
-
-      
       {/* <AuthModal
         trigger={
           <button className="py-2 px-5 rounded-xl cursor-pointer bg-neutral-800">Login</button>
--- components/session/Hero.tsx
+++ components/session/Hero.tsx
@@ -2,13 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='z-20 fixed bottom-0 flex flex-col justify-center items-center w-full gap-2 text-white'>
-      <div className='h-2 w-2 bg-white rounded full'></div>
-      <p className='className="bg-[rgba(64,64,64,0.14)] text-sm border-[0.7px] border-[#2A2A2A] backdrop-blur-[2.8px] rounded-full py-1.5 px-6'>1,252 People are on matrix</p>
-      <p className='text-6xl'>Clarity in <span>Complexity</span></p>
-      <p>We help you to generate everyting help you to generate everyting help you to generate everyting help you to generate everyting.</p>
-      <button className='bg-white rounded-full text-black py-2 px-5'>Get Started</button>
-    </div>
+    <div></div>
   )
 }
 

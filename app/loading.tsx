ccff4d59e540aa175312347f3bbe740119284136
Nakul Chouksey <71.nakul@gmail.com>
2025-09-15 14:45:23 +0530

    moidified: loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,14 @@
-export default function Loading() {
-  return <div>Loading...</div>;
+// components/LogoWrapper.tsx
+"use client";
+
+import Image from "next/image";
+
+export default function LogoWrapper() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center bg-black">
+      <Image src="/logo-anime.svg" alt="Sanka Logo" width={1200} height={1200} className="size-64"/>
+    </div>
+  );
 }
+
+// <Image alt="" src={"/logo.svg"} height={1000} width={1000}/>
